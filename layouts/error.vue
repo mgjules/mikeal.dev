491316e2d371a8629696101002ead8d13b1efd6f
Jules Michael <julesmichaelgiovanni@gmail.com>
2020-06-03 01:43:38 +0400

    error has a button for safety but prettier refact everything

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="container mx-auto flex flex-col justify-center items-center flex-grow">
-    <img src="~/assets/img/error.jpg" alt="wtf?" class="h-40 w-40 object-cover rounded-full" />
+  <div
+    class="container mx-auto flex flex-col justify-center items-center flex-grow"
+  >
+    <img
+      src="~/assets/img/error.jpg"
+      alt="wtf?"
+      class="h-40 w-40 object-cover rounded-full"
+    />
     <h1 class="uppercase font-light text-4xl mt-2">UH OH!</h1>
-    <h2 class="uppercase font-light text-2xl mt-1" v-if="error.statusCode === 404">
+    <h2
+      class="uppercase font-light text-2xl mt-1"
+      v-if="error.statusCode === 404"
+    >
       Page not found
     </h2>
     <h2 class="uppercase font-light text-2xl mt-1" v-else>
       Something bad happened
     </h2>
-    <nuxt-link to="/" class="mt-8">Back to safety</nuxt-link>
+    <BaseButton class="mt-6 text-sm uppercase" @click.native="$router.push('/')"
+      >Back to safety</BaseButton
+    >
   </div>
 </template>
 
