ccc43268a2ba2c3d66320d862c89d6e4c3ee7828
Jules Michael <julesmichaelgiovanni@gmail.com>
2020-06-06 23:52:51 +0400

    bottom post nav

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -31,6 +31,57 @@
       </div>
       <nuxt-content class="mt-8" :document="post" />
     </article>
+    <div
+      class="post-nav container my-10 flex flex-col sm:flex-row items-center sm:justify-between space-y-3 md:space-y-0"
+    >
+      <div>
+        <nuxt-link
+          class="text-xl flex items-center"
+          :to="`/posts/${prev.slug}`"
+          v-if="prev"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            class="fill-current h-8 w-8"
+            x="0px"
+            y="0px"
+            viewBox="0 0 20 20"
+            enable-background="new 0 0 20 20"
+            xml:space="preserve"
+          >
+            <path
+              d="M12.141,13.418c0.268,0.271,0.268,0.709,0,0.978c-0.268,0.27-0.701,0.272-0.969,0l-3.83-3.908  c-0.268-0.27-0.268-0.707,0-0.979l3.83-3.908c0.267-0.27,0.701-0.27,0.969,0c0.268,0.271,0.268,0.709,0,0.978L9,10L12.141,13.418z"
+            />
+          </svg>
+          <div class="break-words">{{ prev.title }}</div></nuxt-link
+        >
+      </div>
+      <div class="hidden sm:block flex-1" v-if="!prev || !next"></div>
+      <div>
+        <nuxt-link
+          class="text-xl flex items-center"
+          :to="`/posts/${next.slug}`"
+          v-if="next"
+          ><div class="break-words">{{ next.title }}</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            class="fill-current h-8 w-8"
+            x="0px"
+            y="0px"
+            viewBox="0 0 20 20"
+            enable-background="new 0 0 20 20"
+            xml:space="preserve"
+          >
+            <path
+              d="M11,10L7.859,6.58c-0.268-0.27-0.268-0.707,0-0.978c0.268-0.27,0.701-0.27,0.969,0l3.83,3.908  c0.268,0.271,0.268,0.709,0,0.979l-3.83,3.908c-0.267,0.272-0.701,0.27-0.969,0c-0.268-0.269-0.268-0.707,0-0.978L11,10z"
+            /></svg
+        ></nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -91,3 +142,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.post-nav {
+  a {
+    &:hover {
+      @apply border-0;
+    }
+  }
+}
+</style>
